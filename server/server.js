93e878e7ef16c11d05eebbf7205fb1862052e445
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-04-02 19:10:51 +0530

    initialized server

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const userRecords = require('./data/users.json');
+const bodyParser = require('body-parser')
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(bodyParser.json());
+
+//Get user records 
+server.get(`/userdb`,(req,res)=>{
+    res.json(userRecords)
+})
+// server.post('/login',(req,res)=>{
+//     res.json({
+//         status: 'sucess',
+//         username,
+//         email,
+//     });
+// })
+// //post the user data 
+// server.post(`/userdb`,(req,res)=>{
+//     const {body} = req;
+//     const {username,email,password} = body;
+//     const user = {
+//         username,
+//         email,
+//         password
+//     }
+//     userRecords.push(user);
+//     // res.json(user)
+//     res.json({
+//         status:'sucess'
+//     })
+// })
+//creating a server
+server.listen(3001,()=>{
+    console.log(`Listening at http://localhost:3001/userdb`)
+})
