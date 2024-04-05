ff6fbf4e02d6bc4ff1a03e6a11cd890b6b7bd161
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-04-05 10:46:54 +0530

    create new component

--- multi-step-login-form/src/page/create-profile-page/index.js
+++ multi-step-login-form/src/page/create-profile-page/index.js
@@ -1,43 +1,43 @@
-import UploadImg from '../../component/upload-img';
-import Logo from '../../component/logo';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import UploadImg from "../../component/upload-img";
+import Logo from "../../component/logo";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 const CreateProfile = () => {
     const navigate = useNavigate();
-    const [location, setLocation] = useState('');
+    const [location, setLocation] = useState("");
     const [isFormFilled, setIsFormFilled] = useState(false)
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        navigate('/login')
+        navigate("/login")
     }
 
     const handleLocationChange = (e) => {
         setLocation(e.target.value);
-        setIsFormFilled(e.target.value !== '');
+        setIsFormFilled(e.target.value !== "");
     }
     return (
         <>
-            <div className="grid gride-cols-1 md:grid-cols-12 pt-10 px-10">
-                <div className='col-span-4 md:col-span-1'>
+            <div className="grid grid-cols-1 md:grid-cols-12 pt-10 px-10">
+                <div className="col-span-4 md:col-span-1">
                     <div className="float-left ">
                         <Logo />
                     </div>
                 </div>
             </div>
             <div className="mx-auto max-w-xl p-4">
-                <div className='block my-6'>
-                    <h2 className="text-3xl text-[#060606] font-bold">Welcome! Let's create your profile</h2>
-                    <p className='mt-2 text-[#757575]'>Let others get to know you better! You can do these later</p>
+                <div className="block my-6">
+                    <h2 className="text-3xl text-[#060606] font-bold">Welcome! Let"s create your profile</h2>
+                    <p className="mt-2 text-[#757575]">Let others get to know you better! You can do these later</p>
                 </div>
-                <form className='block my-6' onSubmit={handleSubmit}>
-                    <div className='my-10 font-bold'>Add an avatar</div>
-                    <div className='my-10'>
+                <form className="block my-6" onSubmit={handleSubmit}>
+                    <div className="my-10 font-bold">Add an avatar</div>
+                    <div className="my-10">
                         <UploadImg />
                     </div>
 
                     <div className="my-10">
-                        <label className='block mb-2 font-bold my-10'>Add your location</label>
+                        <label className="block mb-2 font-bold my-10">Add your location</label>
                         <input type="text" placeholder="Enter a location" className="shadow-md rounded-md w-full px-3 py-2 mt-2" value={location} onChange={handleLocationChange} />
                     </div>
                     <div className="my-10">
--- multi-step-login-form/src/page/type-selection/index.js
+++ multi-step-login-form/src/page/type-selection/index.js
@@ -1,7 +1,26 @@
+import Logo from "../../component/logo";
+
 const TypeSelection = () =>{
     return(
         <>
-        dfgewfygwegf
+        <div className="grid grid-col-1 md:grid-cols-12 pt-10 px-10">
+            <div className="col-span-4 md:col-span-1">
+                <div className="float-left">
+                    <Logo/>
+                </div>
+            </div>
+            <div className="col-span-4 md:col-span-1">
+                <div className="float-left">
+                    
+                </div>
+            </div>
+        </div>
+        <div className="mx-auto max-w-xl p-4">
+            <div className="block my-6">
+                <h2 className="text-3xl text-[#060606] font-bold">What brings you to dribbble?</h2>
+                <p className="mt-2 text-[#757575]">Select the option that describe you. Don't worry, you can explore other option later.</p>
+            </div>
+        </div>
         </>
     )
 }
